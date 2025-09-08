e6229ed905f65151764ff1e0187e70459414399c
tjwombo <tj.itliong@gmail.com>
2025-09-07 19:29:43 -0500

    more more name change

--- BuildLinked.ps1
+++ BuildLinked.ps1
@@ -2,8 +2,8 @@
 Split-Path $MyInvocation.MyCommand.Path | Push-Location
 [Environment]::CurrentDirectory = $PWD
 
-Remove-Item "$env:RELOADEDIIMODS/Rabit_and_Steel_Test/*" -Force -Recurse
-dotnet publish "./Rabit_and_Steel_Test.csproj" -c Release -o "$env:RELOADEDIIMODS/Rabit_and_Steel_Test" /p:OutputPath="./bin/Release" /p:ReloadedILLink="true"
+Remove-Item "$env:RELOADEDIIMODS/RnSArchipelago/*" -Force -Recurse
+dotnet publish "./Rabit_and_Steel_Test.csproj" -c Release -o "$env:RELOADEDIIMODS/RnSArchipelago" /p:OutputPath="./bin/Release" /p:ReloadedILLink="true"
 
 # Restore Working Directory
 Pop-Location
--- Publish.ps1
+++ Publish.ps1
@@ -162,8 +162,8 @@ param (
     $UseScriptDirectory=$True,
 
     ## => User Config <= ## 
-    $ProjectPath = "Rabit_and_Steel_Test.csproj",
-    $PackageName = "Rabit_and_Steel_Test",
+    $ProjectPath = "RnSArchipelago.csproj",
+    $PackageName = "RnSArchipelago",
     $PublishOutputDir = "Publish/ToUpload",
 
     ## => User: Delta Config
@@ -180,7 +180,7 @@ param (
 
     $GameBananaItemId = 333681, # From mod page URL.
 
-    $NuGetPackageId = "Rabit_and_Steel_Test",
+    $NuGetPackageId = "RnSArchipelago",
     $NuGetFeedUrl = "http://packages.sewer56.moe:5000/v3/index.json",
     $NuGetAllowUnlisted = $False,
 
